6194d566a6ce27220eb85e86f7f38619ebf9b4a3
Vincent <danielis.vincent@gmail.com>
2021-11-08 13:03:01 +0100

    Gruppenarbeit 4

---
+++ Gruppenarbeit/GA_4/if_else.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float f1 = 0.5, f2 = 0.3, f3 = 0.2, f4 = f1 - f2 - f3;
+    if(f4 == 0)
+    {
+        printf("is Zero");
+    }
+    else
+    {
+        printf("%.10f is not zero\n", f4);
+    }
+    return 0;
+}
