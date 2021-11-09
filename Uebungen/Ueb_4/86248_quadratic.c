4ec818f60693f46623c32ee26ec748a2ad79a473
Vincent <danielis.vincent@gmail.com>
2021-11-10 00:00:53 +0100

    Quadratic Aufgabe beendet

--- Uebungen/Ueb_4/86248_quadratic.c
+++ Uebungen/Ueb_4/86248_quadratic.c
@@ -1,9 +1,41 @@
 #include "stdio.h"
+#include "math.h"
 
 int main(){
     
-    int day;
-
-    scanf("%d", day);
+    float a, b, c; // Anlegen der einzulesenden Variablen 
+    float result;
+    float resultp, resultn;
+    
+    scanf("%f", &a);    //Einlesen der Variablen 
+    scanf("%f", &b);
+    scanf("%f", &c);
+    
+    float radicant = (b*b) - 4*a*c;
+    
+    if ( a != 0) {
+        if (radicant < 0) {
+            printf("no solution\n");
+        } else if (radicant == 0) {
+            result = -b / (2 * a);
+            printf("%f\n", result);
+        } else {
+            resultp = (-b + sqrt(radicant)) / (2 * a);
+            resultn = (-b - sqrt(radicant)) / (2 * a);
+            printf("%f\n", resultp);
+            printf("%f\n", resultn);
+        }
+    }else{
+        if ( b == 0){
+            if ( c == 0){
+                printf("infinite solutions\n");
+            }else{
+                printf("no solution\n");
+            }
+        }else{
+            result = -(c/b);
+            printf("%f\n", result);
+        }
+    }
     
 }
