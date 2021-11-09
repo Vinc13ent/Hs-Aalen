2d9b4dc481a695dad9ad1d2fd8664e82d65006ce
Vincent <danielis.vincent@gmail.com>
2021-11-09 23:34:46 +0100

    Week Aufgabe beendet

--- Uebungen/Ueb_4/86248_week.c
+++ Uebungen/Ueb_4/86248_week.c
@@ -1,6 +1,40 @@
 ﻿#include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int userinput;
+    scanf("%d", &userinput);
+    switch (userinput) 
+    {
+        case 1: 
+            printf("Monday");
+            break;
+            
+        case 2:
+            printf("Tuesday");
+            break;
+            
+        case 3:
+            printf("Wednesday");
+            break;
+            
+        case 4:
+            printf("Thursday");
+            break;
+            
+        case 5:
+            printf("Friday");
+            break;
+            
+        case 6:
+            printf("Saturday");
+            break;
+            
+        case 7:
+            printf("Sunday");
+            break;
+            
+        default:
+            printf("Unknown");
+    }
     return 0;
 }
