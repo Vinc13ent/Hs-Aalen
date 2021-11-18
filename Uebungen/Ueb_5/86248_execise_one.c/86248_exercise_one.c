d6a12d0fadeb5d6368b68425a6c7c8787bd82dd9
Vincent <danielis.vincent@gmail.com>
2021-11-18 21:14:46 +0100

    Ueb_5 aufgabe 1 done

---
+++ Uebungen/Ueb_5/86248_execise_one.c/86248_exercise_one.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int main() {
+    int entry;
+    scanf("%d", &entry);
+    if (entry == 0){
+        printf("\n");
+    }else if(entry == 1){
+        printf("\n");
+    }else {
+        for (int i = 2; i <= entry; ++i) {
+            if (i % 2 == 0) {
+                if (i == entry) {
+                    printf("%d\n", i);
+                    break;
+                } else if (i == entry - 1) {
+                    printf("%d\n", i);
+                    break;
+                } else {
+                    printf("%d, ", i);
+                }
+            }
+        }
+    }
+   return 0;
+}
