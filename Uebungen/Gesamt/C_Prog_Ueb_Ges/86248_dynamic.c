83fc4a03080b83fdbe1e429dee2d3ef7524ddb5e
Vincent <danielis.vincent@gmail.com>
2021-12-09 23:02:37 +0100

    Aufgabe 08 und neuer ordner mit neuem project in welchem alle aufgaben sind dass alle aufgaben in Clion gleichzeitig drinn sind

---
+++ Uebungen/Gesamt/C_Prog_Ueb_Ges/86248_dynamic.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int size = 0;
+    int i = 0;
+
+    scanf("%d", &size);
+    double *ptr = malloc(size*sizeof (double));
+    
+    if (ptr == 0){
+        return 0;
+    }
+
+    while (i < size){
+        scanf("%lf", &ptr[i]);
+        i++;
+    }
+    
+    i = 0;
+    while (i < size){
+        printf("%lf\n", ptr[i]*2);
+        i++;
+    }
+
+    free(ptr);
+    ptr = NULL;
+    
+    return 0;
+}
---
+++ Uebungen/Gesamt/C_Prog_Ueb_Ges/86248_pointer.c
@@ -0,0 +1,35 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+void read (double* p, unsigned int size){
+    for (int i = 0; i < size; i++) {
+        scanf("%lf", &p[i]);
+    }
+}
+
+void times (double* p, unsigned int size){
+    for (int i = 0; i < size; i++) {
+        p[i] *= 2;         
+    }
+}
+
+void print (double* p, unsigned int size){
+    for (int i = 0; i < size; i++) {
+        printf("%lf\n", p[i]);
+    }
+}
+
+int main (){
+    unsigned int size;
+    scanf("%du", &size);
+    if (size == 0){
+        return 0;
+    }
+    double *p = malloc(size*sizeof (double ));
+    read(p, size);
+    times(p, size);
+    print(p, size);
+    free(p);
+    p = NULL;
+    return 0;
+}
