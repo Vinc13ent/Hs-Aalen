da01b5edc27d9a20f25218d3b557013e9cb2195d
Vincent <danielis.vincent@gmail.com>
2021-12-15 00:29:23 +0100

    count abgeschlossen

---
+++ Uebungen/Gesamt/C_Prog_Ueb_Ges/86248_count.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+    
+    char *zk = argv[1];
+    char zeichen = *argv[2];
+    int count = 0;
+
+    for (int i = 0; i < strlen(zk); i++) {
+        if (zk[i] == zeichen){
+            count++;
+        }
+    }
+    printf("%d", count);
+    
+    return 0;
+}
