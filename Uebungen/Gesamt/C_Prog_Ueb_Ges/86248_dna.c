2fae317724cfea4babd88aecf413a760ee0cf243
Vincent <danielis.vincent@gmail.com>
2021-12-15 01:20:59 +0100

    dna abgeschlossen

---
+++ Uebungen/Gesamt/C_Prog_Ueb_Ges/86248_dna.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void complement( char* dna );
+
+int main(){
+    
+    char orgstrang[100];
+    char compstrang[100];
+    
+    fgets(orgstrang, 100, stdin);
+    strcpy(compstrang, orgstrang);
+
+    complement(compstrang);
+
+    printf("%s", compstrang);
+    printf("%s", orgstrang);
+
+    return 0; 
+}
+
+void complement( char* dna){
+    for (int i = 0; i < strlen(dna); i++) {
+        switch (dna[i]) {
+            case 't': dna[i] = 'A'; break;
+            case 'T': dna[i] = 'A'; break;
+            case 'a': dna[i] = 'T'; break;
+            case 'A': dna[i] = 'T'; break;
+            case 'c': dna[i] = 'G'; break;
+            case 'C': dna[i] = 'G'; break;
+            case 'g': dna[i] = 'C'; break;
+            case 'G': dna[i] = 'C'; break;
+            case '\n': break;
+            case ' ': break;
+            default: dna[i] = ' '; break;
+        }
+    }
+}
